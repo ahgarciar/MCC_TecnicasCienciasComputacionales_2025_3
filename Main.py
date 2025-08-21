fccfbed0853dfeac32a8db4dbde3682a684c8a94
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2025-08-20 20:47:32 -0600

    IntroPython

---
+++ Main.py
@@ -0,0 +1,10 @@
+
+
+import MetodosAuxiliares as auxiliar
+
+
+r = auxiliar.sumar(5, 6)
+print("Resultado: " + str(r))
+
+if __name__ == "__main__":
+    print("hola")
---
+++ MetodosAuxiliares.py
@@ -0,0 +1,10 @@
+
+def sumar(a, b):
+    return a+b
+
+res = sumar(2, 5)
+print("res: " + str(res))
+
+if __name__ == "__main__":
+    print("hola desde auxilar")
+
